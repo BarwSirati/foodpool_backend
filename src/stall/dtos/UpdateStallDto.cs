557a3df3718b6a56a275aed49f761bdf33add9cd
Passamon S <88275001+qpsss@users.noreply.github.com>
2023-04-18 22:31:44 +0700

    Update: remove id in updateDTO

--- src/stall/dtos/UpdateStallDto.cs
+++ src/stall/dtos/UpdateStallDto.cs
@@ -2,7 +2,6 @@ namespace FoodPool.stall.dtos
 {
     public class UpdateStallDto
     {
-            public int Id { get; set; }
-            public string? Name { get; set; }
+        public string? Name { get; set; }
     }
 }
