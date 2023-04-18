4636258e24b6fe9b88754bbe0d2e95fd45b3dcb9
Passamon S <88275001+qpsss@users.noreply.github.com>
2023-04-18 23:57:20 +0700

    Initial: feature/order

---
+++ src/order/entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FoodPool.user.entities;
+
+namespace FoodPool.order.entities;
+
+public enum Status
+{
+    WaitingForConfirmation,
+    HeadingToCafeteria,
+    HeadingToTheDestination,
+    OrderDelivered,
+    OrderCancelled
+}
+
+public class Order
+{
+    [Key] public int Id { get; set; }
+    // public int postId { get; set; }
+    public User? User { get; set; }
+    public string? MenuName { get; set; }
+    public Status Status { get; set; }
+}
