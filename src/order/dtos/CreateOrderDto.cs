1377101ecbc990a50860470ff2a9cb862223adba
BarwSirati <sirati10546@gmail.com>
2023-04-28 15:16:49 +0700

    chore: Datannotatio

--- src/order/dtos/CreateOrderDto.cs
+++ src/order/dtos/CreateOrderDto.cs
@@ -7,5 +7,5 @@ public class CreateOrderDto
     [Required] public int PostId { get; set; }
     [Required] public int UserId { get; set; }
     [Required] public string? MenuName { get; set; }
-    [Required] public string? Note { get; set; }
+    public string? Note { get; set; }
 }
